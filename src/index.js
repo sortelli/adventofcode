3a6b7187f1f1de44e0d47c29f9b2465a297ef42c
Joe Sortelli <joe@sortelli.com>
2015-12-19 03:09:22 -0800

    Trim whitespace out of input files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import fs    from 'fs'
 import day01 from './day_01'
 
 let get_input = function(file) {
-  return fs.readFileSync(`${__dirname}/../data/${file}`, {encoding: 'utf-8'})
+  return fs.readFileSync(`${__dirname}/../data/${file}`, {encoding: 'utf-8'}).trim()
 }
 
 console.log(`Day 01 solution: ${day01(get_input('day_01_input.txt'))}`)
