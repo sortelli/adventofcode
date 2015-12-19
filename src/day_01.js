570d8a3973596140dddd709a1c8e49ceac85fc20
Joe Sortelli <joe@sortelli.com>
2015-12-19 10:06:20 -0800

    Refactor Day 01

--- src/day_01.js
+++ src/day_01.js
@@ -1,16 +1,13 @@
 'use strict'
 
+import _ from 'lodash'
+
 let floors = function(instructions) {
   return instructions.split('')
     .reduce((floors, instruction) => {
-      return floors.concat(floors[floors.length - 1] + (instruction == '(' ? 1 : -1))
+      return floors.concat(_.last(floors) + (instruction == '(' ? 1 : -1))
     }, [0])
 }
 
-export let part1 = function(instructions) {
-  return floors(instructions).pop()
-}
-
-export let part2 = function(instructions) {
-  return floors(instructions).indexOf(-1)
-}
+export let part1 = (instructions) => floors(instructions).pop()
+export let part2 = (instructions) => floors(instructions).indexOf(-1)
