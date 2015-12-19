cc68f729545cec5016097abc0a672f56191d776f
Joe Sortelli <joe@sortelli.com>
2015-12-19 10:22:28 -0800

    Add test for Day 02 Part 1

---
+++ src/day_02.js
@@ -0,0 +1,3 @@
+'use strict'
+
+export let part1 = () => null
---
+++ test/test_day_02.js
@@ -0,0 +1,19 @@
+'use strict'
+
+import util       from 'util'
+import { expect } from 'chai'
+import * as day02 from '../src/day_02'
+
+describe('Day 02', function() {
+  describe('Part 1', function() {
+    [
+      {input: '2x3x4',         output:   58},
+      {input: '1x1x10',        output:   43},
+      {input: "2x3x4\n1x1x10", output:  101}
+    ].forEach(({input: input, output: output}) => {
+      it(`passes ${util.inspect(input)} => ${output}`, () => {
+        expect(day02.part1(input)).to.equal(output)
+      })
+    })
+  })
+})
