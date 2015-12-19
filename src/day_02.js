2c29a90dd8dd2dc80b7246e873bccc1aac092c31
Joe Sortelli <joe@sortelli.com>
2015-12-19 10:43:41 -0800

    Add solution for Day 02 Part 1

--- src/day_02.js
+++ src/day_02.js
@@ -1,3 +1,10 @@
 'use strict'
 
-export let part1 = () => null
+import util from 'util'
+import _    from 'lodash'
+
+export let part1 = function(dimensions) {
+  return dimensions.split("\n").map((line) => line.split('x')).map(([l, w, h]) => {
+    return 2*l*w + 2*w*h + 2*l*h + _.min([l*w, w*h, l*h])
+  }).reduce((sum, size) => sum + size)
+}
--- test/test_day_02.js
+++ test/test_day_02.js
@@ -9,7 +9,10 @@ describe('Day 02', function() {
     [
       {input: '2x3x4',         output:   58},
       {input: '1x1x10',        output:   43},
-      {input: "2x3x4\n1x1x10", output:  101}
+      {input: "2x3x4\n1x1x10", output:  101},
+      {input: '3x4x2',         output:   58},
+      {input: '1x10x1',        output:   43},
+      {input: "3x4x2\n10x1x1", output:  101}
     ].forEach(({input: input, output: output}) => {
       it(`passes ${util.inspect(input)} => ${output}`, () => {
         expect(day02.part1(input)).to.equal(output)
