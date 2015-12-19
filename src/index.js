265fa3e75d7029e2c5af39424ddc915d92373c6c
Joe Sortelli <joe@sortelli.com>
2015-12-19 13:59:45 -0800

    Add test for Day 04 Part 1

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import fs         from 'fs'
 import * as day01 from './day_01'
 import * as day02 from './day_02'
 import * as day03 from './day_03'
+import * as day04 from './day_04'
 
 let get_input = function(file) {
   return fs.readFileSync(`${__dirname}/../data/${file}`, {encoding: 'utf-8'}).trim()
@@ -20,3 +21,6 @@ console.log(`Day 02 part 2 solution: ${day02.part2(day_02_input)}`)
 let day_03_input = get_input('day_03_input.txt')
 console.log(`Day 03 part 1 solution: ${day03.part1(day_03_input)}`)
 console.log(`Day 03 part 2 solution: ${day03.part2(day_03_input)}`)
+
+let day_04_input = get_input('day_04_input.txt')
+console.log(`Day 04 part 1 solution: ${day04.part1(day_04_input)}`)
---
+++ test/test_day_04.js
@@ -0,0 +1,17 @@
+'use strict'
+
+import { expect } from 'chai'
+import * as day04 from '../src/day_04'
+
+describe('Day 04', function() {
+  describe('Part 1', function() {
+    [
+      {input: 'abcdef',  output:  609043},
+      {input: 'pqrstuv', output: 1048970}
+    ].forEach(({input: input, output: output}) => {
+      it(`passes ${input} => ${output}`, () => {
+        expect(day04.part1(input)).to.equal(output)
+      })
+    })
+  })
+})
