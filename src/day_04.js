cfae283532f7698652846d263aa05c98da55273d
Joe Sortelli <joe@sortelli.com>
2015-12-19 14:13:54 -0800

    Add solution for Day 04 Part 1

--- src/day_04.js
+++ src/day_04.js
@@ -1,3 +1,10 @@
 'use strict'
 
-export let part1 = () => null
+import md5 from 'md5'
+
+export let part1 = (secret) => {
+  for (let n = 1; true; ++n) {
+    if (md5(secret + n).startsWith('00000'))
+      return n
+  }
+}
--- test/test_day_04.js
+++ test/test_day_04.js
@@ -5,6 +5,8 @@ import * as day04 from '../src/day_04'
 
 describe('Day 04', function() {
   describe('Part 1', function() {
+    this.timeout(10000);
+
     [
       {input: 'abcdef',  output:  609043},
       {input: 'pqrstuv', output: 1048970}
