0be18af6f1f2be4319f4fcd135f44b369f28166d
Joe Sortelli <joe@sortelli.com>
2015-12-19 15:34:49 -0800

    Parameterize index.js for running specific days

--- src/index.js
+++ src/index.js
@@ -1,27 +1,40 @@
 'use strict'
 
-import fs         from 'fs'
-import * as day01 from './day_01'
-import * as day02 from './day_02'
-import * as day03 from './day_03'
-import * as day04 from './day_04'
+import fs from 'fs'
+import _  from 'lodash'
+
+import * as day_01 from './day_01'
+import * as day_02 from './day_02'
+import * as day_03 from './day_03'
+import * as day_04 from './day_04'
+
+let days = [
+  day_01,
+  day_02,
+  day_03,
+  day_04
+]
 
 let get_input = function(file) {
   return fs.readFileSync(`${__dirname}/../data/${file}`, {encoding: 'utf-8'}).trim()
 }
 
-let day_01_input = get_input('day_01_input.txt')
-console.log(`Day 01 part 1 solution: ${day01.part1(day_01_input)}`)
-console.log(`Day 01 part 2 solution: ${day01.part2(day_01_input)}`)
+let run_day = (day_num) => {
+  day_num = parseInt(day_num, 10)
 
-let day_02_input = get_input('day_02_input.txt')
-console.log(`Day 02 part 1 solution: ${day02.part1(day_02_input)}`)
-console.log(`Day 02 part 2 solution: ${day02.part2(day_02_input)}`)
+  let day_str = _.padLeft(day_num, 2, '0')
+  let input   = get_input(`day_${day_str}_input.txt`);
 
-let day_03_input = get_input('day_03_input.txt')
-console.log(`Day 03 part 1 solution: ${day03.part1(day_03_input)}`)
-console.log(`Day 03 part 2 solution: ${day03.part2(day_03_input)}`)
+  ['part1', 'part2'].forEach((part) => {
+    let day = days[day_num - 1]
 
-let day_04_input = get_input('day_04_input.txt')
-console.log(`Day 04 part 1 solution: ${day04.part1(day_04_input)}`)
-console.log(`Day 04 part 2 solution: ${day04.part2(day_04_input)}`)
+    if (day && typeof day[part] == 'function')
+      console.log(`Day ${day_str} ${part}() = ${day[part](input)}`)
+  })
+}
+
+let args = process.argv.length > 2 ? process.argv.slice(2) : days.map((d, n) => n + 1)
+
+args.forEach((day_num) => {
+  run_day(day_num)
+})
