36cf4b6e13237aef37e9c5c25d14595ed2056670
Joe Sortelli <joe@sortelli.com>
2015-12-20 01:53:39 -0800

    Add test for Day 08 Part 2

--- test/test_day_08.js
+++ test/test_day_08.js
@@ -16,4 +16,17 @@ describe('Day 08', function() {
       expect(day08.part1(inputs.join("\n"))).to.equal(12)
     })
   })
+
+  describe('Part 2', function() {
+    let inputs = [
+      '""',
+      '"abc"',
+      '"aaa\\"aaa"',
+      '\"\\x27\"'
+    ]
+
+    it(`Passes ${inputs.join(', ')}`, () => {
+      expect(day08.part2(inputs.join("\n"))).to.equal(19)
+    })
+  })
 })
