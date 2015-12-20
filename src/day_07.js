2b17276f3e03af56bea5ef7b88075086f6bcccc2
Joe Sortelli <joe@sortelli.com>
2015-12-19 22:07:10 -0800

    Add solution for Day 07 Part 1

--- src/day_07.js
+++ src/day_07.js
@@ -1,5 +1,55 @@
 'use strict'
 
-export let eval_circuit = (circuit) => ({})
+import _ from 'lodash'
+
+let once = (func) => {
+  let value
+
+  return () => {
+    if (typeof(value) != 'undefined')
+      return value
+
+    return value = func()
+  }
+}
+
+let read = function(wires, name) {
+  if (name.match(/^\d+$/))
+    return parseInt(name, 10)
+
+  return wires[name]()
+}
+
+let parsers = [
+  [/^(\w+)\s*->\s*(\w+)$/,                  (wires, a, b)    => wires[b] = once(() => read(wires, a)                            )],
+  [/^NOT\s+(\w+)\s*->\s*(\w+)$/,            (wires, a, b)    => wires[b] = once(() => (65535 - read(wires, a))                  )],
+  [/^(\w+)\s+AND\s+(\w+)\s*->\s*(\w+)$/,    (wires, a, b, c) => wires[c] = once(() => (read(wires, a) &  read(wires, b))        )],
+  [/^(\w+)\s+OR\s+(\w+)\s*->\s*(\w+)$/,     (wires, a, b, c) => wires[c] = once(() => (read(wires, a) |  read(wires, b))        )],
+  [/^(\w+)\s+LSHIFT\s+(\w+)\s*->\s*(\w+)$/, (wires, a, b, c) => wires[c] = once(() => (read(wires, a) << read(wires, b)) & 65535)],
+  [/^(\w+)\s+RSHIFT\s+(\w+)\s*->\s*(\w+)$/, (wires, a, b, c) => wires[c] = once(() => (read(wires, a) >> read(wires, b))        )]
+]
+
+let set_wires = (wires, str) => {
+  return ([regex, setter]) => {
+    let match = str.match(regex)
+    if (match)
+      setter(wires, match[1], match[2], match[3])
+    return match
+  }
+}
+
+let parse_circuit = (wires, str) => {
+  if (!_.some(parsers, set_wires(wires, str)))
+    throw new Error('Unknown circuit: ' + str)
+  return wires
+}
+
+let wire_circuit = (circuit) => circuit.split("\n").map((s) => s.trim()).reduce(parse_circuit, {})
+
+export let eval_circuit = (circuit) => {
+  return _.transform(wire_circuit(circuit) , (wires, value, key) => {
+      wires[key] = value()
+  })
+}
 
 export let part1 = (circuit) => eval_circuit(circuit).a
--- test/test_day_07.js
+++ test/test_day_07.js
@@ -5,14 +5,29 @@ import { expect } from 'chai'
 import * as day07 from '../src/day_07'
 
 describe('Day 07', function() {
+  describe('eval_circuit()', function() {
+    it('simple input', () => {
+      expect(day07.eval_circuit("456 -> x")).to.have.property('x', 456)
+      expect(day07.eval_circuit("456 -> x\nx -> y")).to.have.property('y', 456)
+      expect(day07.eval_circuit("NOT 456 -> x")).to.have.property('x', 65079)
+      expect(day07.eval_circuit("123 -> x\nNOT x -> y")).to.have.property('y', 65412)
+      expect(day07.eval_circuit("123->x\n456->y\nx AND y -> z")).to.have.property('z', 123 & 456)
+      expect(day07.eval_circuit("123->x\n456->y\nx OR y -> z")).to.have.property('z', 123 | 456)
+      expect(day07.eval_circuit("123->x\n2->y\nx LSHIFT y -> z")).to.have.property('z', 123 << 2)
+      expect(day07.eval_circuit("65530->x\n2->y\nx LSHIFT y -> z")).to.have.property('z', 65512)
+      expect(day07.eval_circuit("123->x\n2->y\nx RSHIFT y -> z")).to.have.property('z', 123 >> 2)
+      expect(() => day07.eval_circuit("123 FOO x -> z")).to.throw(/Unknown circuit/)
+    })
+  })
+
   describe('Part 1', function() {
     let input, output
 
     before(() => {
       input = `123 -> x
-               456 -> y
                789 -> z
                x AND y -> a
+               456 -> y
                x OR y -> e
                x LSHIFT 2 -> f
                y RSHIFT 2 -> g
