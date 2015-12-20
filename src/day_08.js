26a25d87b42e8f43c204a0e6b7a895788c4b7870
Joe Sortelli <joe@sortelli.com>
2015-12-20 01:38:39 -0800

    Add solution for Day 08 Part 1

--- src/day_08.js
+++ src/day_08.js
@@ -1,3 +1,11 @@
 'use strict'
 
-export let part1 = () => null
+let size = (input, sizer) => input.map(sizer).reduce((sum, length) => sum + length)
+
+export let part1 = (strings) => {
+  let input   = strings.split("\n").map((s) => s.trim())
+  let on_disk = size(input, (s) => s.length      )
+  let in_mem  = size(input, (s) => eval(s).length)
+
+  return on_disk - in_mem
+}
