5a2c0ad5dc7d0fa9a74ab91e49f4bc34b050aa00
Joe Sortelli <joe@sortelli.com>
2015-12-20 11:55:07 -0800

    Add test for Day 09 Part 2

--- test/test_day_09.js
+++ test/test_day_09.js
@@ -4,16 +4,22 @@ import { expect } from 'chai'
 import * as day09 from '../src/day_09'
 
 describe('Day 09', function() {
-  describe('Part 1', function() {
-    let inputs = [
-      'foobar',
-      'London to Dublin = 464',
-      'London to Belfast = 518',
-      'Dublin to Belfast = 141'
-   ]
+  let inputs = [
+    'foobar',
+    'London to Dublin = 464',
+    'London to Belfast = 518',
+    'Dublin to Belfast = 141'
+  ]
 
+  describe('Part 1', function() {
     it(`Passes ${inputs.join(', ')}`, () => {
       expect(day09.part1(inputs.join("\n"))).to.equal(605)
+    })
+  })
+
+  describe('Part 2', function() {
+    it(`Passes ${inputs.join(', ')}`, () => {
+      expect(day09.part2(inputs.join("\n"))).to.equal(982)
     })
   })
 })
