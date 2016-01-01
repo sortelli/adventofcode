3914f3c0828fb5448e79bc6e99479e64f9e11b9a
Joe Sortelli <joe@sortelli.com>
2016-01-01 15:23:37 -0800

    Add solution for Day 12 Part 1

--- src/day_12.js
+++ src/day_12.js
@@ -1,3 +1,14 @@
 'use strict'
 
-export let part1 = () => null
+import _ from 'lodash'
+
+export let sum_numbers = (obj) => {
+  if (typeof obj == 'object')
+    return _.reduce(obj, (sum, value) => sum + sum_numbers(value), 0)
+  else if (typeof obj == 'number')
+    return obj
+  else
+    return 0
+}
+
+export let part1 = (json) => sum_numbers(JSON.parse(json))
