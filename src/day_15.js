de050c212ef3b1cf2620c6479eabb50ee5fea888
Joe Sortelli <joe@sortelli.com>
2016-01-03 22:00:24 -0800

    Add solution for Day 15 Part 1

--- src/day_15.js
+++ src/day_15.js
@@ -1,3 +1,68 @@
 'use strict'
 
-export let part1 = () => null
+import _ from 'lodash'
+
+let parse_ingredients = (input) => {
+  return _.map(input.split("\n"), (line) => {
+    let name = line.substring(0, line.indexOf(':'))
+    let [capacity, durability, flavor, texture, calories] =
+      _.map(line.match(/(-?\d+)/g), (n) => parseInt(n, 10))
+
+    return {name, capacity, durability, flavor, texture, calories}
+  })
+}
+
+export let all_recipes = (width, count) => {
+  if (width == 1)
+    return [[count]]
+
+  let is_new_recipe = (() => {
+    let seen = new Set()
+
+    return (recipe) => {
+      let key = recipe.join(',')
+
+      if (seen.has(key))
+        return false
+
+      seen.add(key)
+      return true
+    }
+  })()
+
+  let new_recipes = []
+
+  all_recipes(width - 1, count).forEach((recipe) => {
+    _.times(count + 1, (i) => {
+      _.times(recipe.length, (j) => {
+        if (recipe[j] - i >= 0) {
+          let tmp = recipe.slice(0)
+          tmp[j] = tmp[j] - i
+          let new_recipe = [i].concat(tmp)
+          if (is_new_recipe(new_recipe))
+            new_recipes.push(new_recipe)
+        }
+      })
+    })
+  })
+
+  return new_recipes
+}
+
+export let part1 = (input) => {
+  let ingredients = parse_ingredients(input)
+  let recipes     = all_recipes(ingredients.length, 100)
+  let params      = ['capacity', 'durability', 'flavor', 'texture']
+
+  return _.reduce(recipes, (max_score, recipe) => {
+    let score = _.zip(..._.map(recipe, (count, index) => {
+      let ingredient = ingredients[index]
+      return _.map(params, (param) => ingredient[param] * count)
+    }))
+    .map((totals) => _.reduce(totals, (sum, total) => sum + total))
+    .map((total) => total < 0 ? 0 : total)
+    .reduce((product, total) => product * total)
+
+    return max_score > score ? max_score : score
+  }, 0)
+}
--- test/test_day_15.js
+++ test/test_day_15.js
@@ -1,5 +1,6 @@
 'use strict'
 
+import _ from 'lodash'
 import { expect } from 'chai'
 import * as day15 from '../src/day_15'
 
@@ -8,6 +9,18 @@ describe('Day 15', function() {
     'Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8',
     'Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3'
   ].join("\n")
+
+  describe('all_recipies()', function() {
+    it('works with 3 types chose 4', () => {
+      let recipes = day15.all_recipes(3, 4)
+      expect(recipes.length).to.equal(15)
+      expect(_.map(recipes, (recipe) => recipe.join(',')).sort()).to.deep.equal([
+	"0,0,4", "0,1,3", "0,2,2", "0,3,1", "0,4,0", "1,0,3",
+	"1,1,2", "1,2,1", "1,3,0", "2,0,2", "2,1,1", "2,2,0",
+	"3,0,1", "3,1,0", "4,0,0"
+      ])
+    })
+  })
 
   describe('Part 1', function() {
     it('passes sample input', () => {
