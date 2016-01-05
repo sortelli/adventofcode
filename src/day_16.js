3238f99909c58d984a437ffee52294ec52b1759d
Joe Sortelli <joe@sortelli.com>
2016-01-04 23:31:48 -0800

    Add solution for Day 16 Part 1

--- src/day_16.js
+++ src/day_16.js
@@ -1,4 +1,31 @@
 'use strict'
 
-export let parse_aunts = () => null
-export let part1 = () => null
+import _ from 'lodash'
+
+let analysis = {
+  children: 3, cats:        7,
+  samoyeds: 2, pomeranians: 3,
+  akitas:   0, vizslas:     0,
+  goldfish: 5, trees:       3,
+  cars:     2, perfumes:    1
+}
+
+export let parse_aunts = (input) => {
+  return _.map(input.split("\n"), (line) => {
+    let params = line.substring(line.indexOf(':') + 2).match(/\w+: \d+/g)
+    return _.reduce(params, (aunt, param) => {
+      let [name, value] = param.split(': ')
+      aunt[name] = parseInt(value, 10)
+      return aunt
+    }, {})
+  })
+}
+
+export let part1 = (input) => {
+  let aunts = parse_aunts(input)
+
+  for (let i = 0; i < aunts.length; ++i) {
+    if (_.all(aunts[i], (value, name) => analysis[name] === value))
+      return i + 1
+  }
+}
--- test/test_day_16.js
+++ test/test_day_16.js
@@ -7,7 +7,8 @@ describe('Day 16', function() {
   let input = [
     'Sue 1: goldfish: 6, trees: 9, akitas: 0',
     'Sue 2: goldfish: 7, trees: 1, akitas: 0',
-    'Sue 3: cars: 10, akitas: 6, perfumes: 7'
+    'Sue 3: cats: 7, vizslas: 0, cars: 2',
+    'Sue 4: cars: 10, akitas: 6, perfumes: 7'
   ].join("\n")
 
   describe('parse_aunts()', function() {
@@ -15,14 +16,15 @@ describe('Day 16', function() {
       expect(day16.parse_aunts(input)).to.deep.equal([
         {goldfish: 6, trees: 9, akitas: 0},
         {goldfish: 7, trees: 1, akitas: 0},
+        {cats: 7, vizslas: 0, cars: 2},
         {cars: 10, akitas: 6, perfumes: 7}
       ])
     })
   })
 
   describe('Part 1', function() {
-    it('returns an int', () => {
-      expect(day16.part1(input)).to.be.a('number')
+    it('passes sample input', () => {
+      expect(day16.part1(input)).to.equal(3)
     })
   })
 })
