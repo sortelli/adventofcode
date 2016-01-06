fce24d29fca66791d27b4699d476479f74e569c2
Joe Sortelli <joe@sortelli.com>
2016-01-05 23:33:50 -0800

    Add test for Day 17 Part 1

---
+++ test/test_day_17.js
@@ -0,0 +1,15 @@
+'use strict'
+
+import { expect } from 'chai'
+import * as day17 from '../src/day_17'
+
+describe('Day 17', function() {
+  let input = ['20', '15', '10', '5', '5'].join("\n")
+
+  describe('Part 1', function() {
+    it('passes sample input', () => {
+      expect(day17.part1(input, 25)).to.equal(4)
+      expect(day17.part1(input)).to.equal(0)
+    })
+  })
+})
