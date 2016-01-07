92b09a2bbee14d57653c33a8ca6536529fd19fcb
Joe Sortelli <joe@sortelli.com>
2016-01-06 17:59:16 -0800

    Add solution for Day 17 Part 1

--- src/day_17.js
+++ src/day_17.js
@@ -1,3 +1,15 @@
 'use strict'
 
-export let part1 = () => null
+import _ from 'lodash'
+import combinatorics from 'js-combinatorics'
+
+let parse_containers = (input) => _.map(input.split("\n"), (line) => parseInt(line, 10))
+
+let count_combinations = (containers, target) => {
+  return combinatorics
+    .power(containers)
+    .filter((cans) => cans.reduce((sum, can) => sum + can, 0) == target)
+    .length
+}
+
+export let part1 = (input, target = 150) => count_combinations(parse_containers(input), target)
