439afde42b19f40d58cadd940f04b403deda56fc
Joe Sortelli <joe@sortelli.com>
2016-01-08 00:39:54 -0800

    Add solution for Day 20 Part 1

--- src/day_20.js
+++ src/day_20.js
@@ -1,4 +1,23 @@
 'use strict'
 
-export let presents = () => null
-export let part1 = () => null
+export let presents = (house) => {
+  let out = 0
+
+  for (let h = house; h > 0; --h) {
+    if (house % h == 0)
+      out += 10 * h
+  }
+
+  return out
+}
+
+export let part1 = (input) => {
+  let target = parseInt(input, 10)
+
+  for (let house = 1; ; ++house) {
+    let out = presents(house)
+
+    if (out >= target)
+      return house
+  }
+}
